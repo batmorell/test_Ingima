9ed1392614993e8ec3573d80c8dcb0a0b89d680f
batmorell <baptiste.morellab@gmail.com>
2017-02-05 00:34:02 +0100

    updating routes into node server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const path = require('path');
+const Client = require('node-rest-client').Client;
 
 /* eslint-disable no-console */
 
 const port = process.env.PORT || 3000;
 const app = express();
+const client = new Client();
 
 app.use(express.static(path.join( __dirname, '../public')));
+
+app.get('/api/search', function(req, res) {
+
+  client.get("https://api.spotify.com/v1/search?q=tania%20bowra&type=artist", function (data, response) {
+    res.json(data.artists);
+  });
+
+});
+
+app.get('/api/artist/:id', function(req, res) {
+
+  client.get("https://api.spotify.com/v1/artists/" +req.params.id +"/albums", function (data, response) {
+    res.json(data);
+  });
+
+});
+
+app.get('/api/album/:id', function(req, res) {
+
+  client.get("https://api.spotify.com/v1/albums/" + req.params.id, function (data, response) {
+    res.json(data);
+  });
+
+});
 
 app.get('*', function (req, res) {
   res.sendFile(path.join( __dirname, '../public/index.html'));
